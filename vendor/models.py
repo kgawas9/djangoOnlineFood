429b9040f68c36047a339fb7855547478a5f9e6f
Kiran <kgawas9@gmail.com>
2022-09-25 23:54:36 +0530

    Vendor approval changes

--- vendor/models.py
+++ vendor/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import User, UserProfile
 
+from accounts.utils import send_vendor_notification
 
 # Create your models here.
 class Vendor(models.Model):
@@ -16,3 +17,29 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.vendor_name
+
+    def save(self, *args, **kwargs):
+        # to identify if update is happening or not
+        if self.pk is not None:
+            original_status = Vendor.objects.get(pk=self.pk)
+            if original_status.is_approved != self.is_approved:
+                if self.is_approved == True:
+                    # send notification email
+                    mail_subject = 'Congratulations! Your restaurant is approved.. you can build your menu now..'
+                else:
+                    # send notification email
+                    mail_subject = 'We are sorry! You are not eligible for publishing your food menu on our marketplace.'
+            
+            else:
+                if self.is_approved == False:
+                    mail_subject = 'foodOnline - Eligibility Criteria Failed'
+
+            
+            mail_template = 'accounts/emails/admin_approval_email.html'
+            context = {
+                'user': self.user,
+                'is_approved': self.is_approved,
+            }
+            send_vendor_notification(mail_subject, mail_template, context)
+
+        return super(Vendor, self).save(*args, **kwargs)
