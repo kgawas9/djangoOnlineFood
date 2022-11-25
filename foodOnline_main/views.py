1bd7817fe31dfb3200601a7c6a3cc4013459b0a5
Kiran <kgawas9@gmail.com>
2022-11-25 15:14:59 +0530

    Nearby restaurants to main page based on current location - 2 and url allocation for hope page vendors

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -7,12 +7,33 @@ from django.contrib.gis.measure import D # 'D is shortcut for distance
 from django.contrib.gis.db.models.functions import Distance
 
 
-def home(request):
-    if 'lat' in request.GET:
+def get_or_set_current_location(request):
+    if 'lat' in request.session:
+        latitude = request.session['lat']
+        longitude = request.session['lng']
+
+        return longitude, latitude 
+
+    elif 'lat' in request.GET:
         latitude = request.GET.get('lat')
         longitude = request.GET.get('lng')
 
-        pnt = GEOSGeometry('POINT(%s %s)' %(longitude, latitude))
+        request.session['lat'] = latitude
+        request.session['lng'] = longitude
+
+        return longitude, latitude
+    
+    else:
+        return None
+
+
+
+def home(request):
+    if get_or_set_current_location(request) is not None:
+        # latitude = request.GET.get('lat')
+        # longitude = request.GET.get('lng')
+
+        pnt = GEOSGeometry('POINT(%s %s)' %(get_or_set_current_location(request)))
 
         vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt, D(km=100))
                                             ).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
