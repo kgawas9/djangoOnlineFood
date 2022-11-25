6c75825d45ba0cf80c780a1cb67052ba0e16500a
Kiran <kgawas9@gmail.com>
2022-11-25 14:50:48 +0530

    Nearby restaurants to main page based on current location - 1

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import render
 from vendor.models import Vendor
 
+# For location
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D # 'D is shortcut for distance
+from django.contrib.gis.db.models.functions import Distance
+
+
 def home(request):
-    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+    if 'lat' in request.GET:
+        latitude = request.GET.get('lat')
+        longitude = request.GET.get('lng')
+
+        pnt = GEOSGeometry('POINT(%s %s)' %(longitude, latitude))
+
+        vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt, D(km=100))
+                                            ).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
+
+        for v in vendors:
+            v.kms = round(v.distance.km,2)
+    
+    else:
+        vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+    
     context = {
         'vendor_list': vendors,
     }
